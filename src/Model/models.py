a6422d0f3f4e7c5d05e665102f6d62dd875899ac
DerNunja <konrad.ammerich@outlook.de>
2025-05-22 23:34:26 +0200

    Update models.py

--- src/Model/models.py
+++ src/Model/models.py
@@ -43,6 +43,5 @@ y = df['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
     
-rf_model.fit(X_train, y_train)
-    
+rf_model.fit(X_train, y_train)    
 dt_model.fit(X_train, y_train)
