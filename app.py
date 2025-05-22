4672da3af7e395197c25c833a1b2f70ca4969b20
Nichibotsu <nbesecke05@gmail.com>
2025-05-22 20:56:45 +0200

    test

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+from src.Model.models import train
 from src.HtmlLayout.index import app
 
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Modelle
+    train()
+    
+    app.run()
--- src/Model/models.py
+++ src/Model/models.py
@@ -8,25 +8,47 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.metrics import confusion_matrix, classification_report, roc_curve, auc
 
+# Filepaths
+FILE_PATH='Datensaetze/heloc_openml.csv'
 
-try:
-    df = pd.read_csv('heloc_openml.csv')
-    df = df.replace('Special', np.nan)
-    for col in df.columns:
-        if col != 'RiskPerformance':
-            df[col] = pd.to_numeric(df[col], errors='coerce')
-    df['target'] = df['RiskPerformance'].map({'Good': 1, 'Bad': 0})
-except FileNotFoundError:
-    print("Datei konnte nicht geladen werden")    
+def Datenerstellung():
 
-features = [col for col in df.columns if col != 'target' and col != 'RiskPerformance']
-X = df[features]
-y = df['target']
+    try:
+            df = pd.read_csv(FILE_PATH)
+            df = df.replace('Special', np.nan)
+            for col in df.columns:
+                if col != 'RiskPerformance':
+                    df[col] = pd.to_numeric(df[col], errors='coerce')
+            df['target'] = df['RiskPerformance'].map({'Good': 1, 'Bad': 0})
+            return df
+            
+    except FileNotFoundError:
+            print("Datei konnte nicht geladen werden") 
+            return None
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-rf_model = RandomForestClassifier(n_estimators=1000, random_state=42)
-rf_model.fit(X_train, y_train)
 
+
+#Pandas DATA HANDLING
+df=Datenerstellung()
+
+# Modelle
 dt_model = DecisionTreeClassifier(max_depth=7, random_state=42)
-dt_model.fit(X_train, y_train)
+rf_model = RandomForestClassifier(n_estimators=1000, random_state=42)
+
+
+def train():
+       
+
+    features = [col for col in df.columns if col != 'target' and col != 'RiskPerformance']
+    X = df[features]
+    y = df['target']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    
+    rf_model.fit(X_train, y_train)
+    
+    dt_model.fit(X_train, y_train)
+    
+    print("Train fertig")
